87b5132752d3f9afc5efa584695297618cce6ce6
Vasilis <petsalakisv@gmail.com>
2026-01-09 06:57:47 +0200

    adding a landing page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+http_response_code(200);
+header('Content-Type: text/plain; charset=utf-8');
+
+echo "ADISE 2025 – Project API\n";
+echo "-----------------------\n";
+echo "This project exposes a REST-like API.\n\n";
+echo "Use curl or a REST client to interact with it.\n";
+echo "Info Resource:\n";
+echo "https://github.com/iee-ihu-gr-course1941/ADISE25_kseri/blob/main/README.md\n";
