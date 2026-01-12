24aaaccea4e47494c269e0c97c3d56a82236c26d
PETSALAKIS Vasilis <vpetsala@netcompany.com>
2026-01-12 09:39:49 +0200

    landing page changes

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
+?>
 
-echo "ADISE 2025 – Project API\n";
-echo "-----------------------\n";
-echo "This project exposes a REST-like API.\n\n";
-echo "Use curl or a REST client to interact with it.\n";
-echo "Info Resource:\n";
-echo "https://github.com/iee-ihu-gr-course1941/ADISE25_kseri/blob/main/README.md\n";
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>ADISE 2025 - API Project</title>
+</head>
+<body>
+    <h3>ADISE 2025 – Project API</h3>
+    <p>This project exposes a REST-like API.</p>
+    <p>Use <code>curl</code> or a REST client to interact with it.</p>
+
+    <p>
+        Documentation:<br>
+        <a href="https://github.com/iee-ihu-gr-course1941/ADISE25_kseri/blob/main/README.md">
+            Github README
+        </a>
+    </p>
+</body>
+</html>
